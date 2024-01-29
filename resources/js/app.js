3b20b03234476562e892b9f9be06b96305afca9b
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-29 17:41:33 +0100

    feat: bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,5 +3,8 @@ import "./bootstrap";
 // custom css
 import "~resources/scss/app.scss";
 
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
 // processi img resources/img/
 import.meta.glob(["../img/**"]);
