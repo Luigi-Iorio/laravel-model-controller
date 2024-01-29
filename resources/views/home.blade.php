1e312acc41ce61659b34b3782bb2a532b682052b
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-29 17:48:20 +0100

    feat: laravel model controller scaffolding

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Template Laravel</title>
+    <title>Laravel Model Controller</title>
     @vite('resources/js/app.js')
 </head>
 
 <body>
     <main>
-        <h1>Template-Laravel-SCSS</h1>
-        <img src="{{ Vite::asset('resources/img/laravel-logo.svg') }}" alt="logo-laravel">
     </main>
 </body>
 
